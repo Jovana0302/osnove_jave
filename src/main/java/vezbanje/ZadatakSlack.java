1e412d1b11ecd2c667a468dc2629bef20c039ef1
Jovana Cvetanovic <jovana.cvetanovic0302@gmail.com>
2022-12-14 17:19:01 +0100

    ZadatakSlack

---
+++ src/main/java/vezbanje/ZadatakSlack.java
@@ -0,0 +1,24 @@
+package vezbanje;
+
+public class ZadatakSlack {
+    public static void main(String[] args) {
+        char filled = '*';
+
+        System.out.println("                    @@@@@");
+        System.out.println("                   @@"+filled+filled+filled+"@@");
+        System.out.println("                  @@@"+filled+filled+filled+filled+"@@");
+        System.out.println("                 @@"+filled+filled+filled+filled+filled+"@@");
+        System.out.println("                @@@"+filled+filled+filled+filled+filled+"@@");
+        System.out.println("               @@@"+filled+filled+filled+filled+filled+filled+"@@");
+        System.out.println("              @@"+filled+filled+filled+filled+filled+filled+filled+filled+"@@@@@@@@@@@@@ ");
+        System.out.println("  @@@@@@@@@ @@@"+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+"@@@");
+        System.out.println("@@"+filled+filled+filled+filled+filled+filled+filled+filled+filled+"@@"+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+"@@");
+        System.out.println("@@"+filled+filled+filled+filled+filled+filled+filled+filled+filled+"@@"+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+"@@");
+        System.out.println("@@"+filled+filled+filled+filled+filled+filled+filled+filled+filled+"@@"+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+"@@");
+        System.out.println("@@"+filled+filled+filled+filled+filled+filled+filled+filled+filled+"@@"+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+"@@");
+        System.out.println("@@"+filled+filled+filled+filled+filled+filled+filled+filled+filled+"@@"+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+"@@");
+        System.out.println("@@"+filled+filled+filled+filled+filled+filled+filled+filled+filled+"@@"+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+"@@");
+        System.out.println("@@"+filled+filled+filled+filled+filled+filled+filled+filled+filled+"@@"+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+filled+"@@");
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+    }
+}
