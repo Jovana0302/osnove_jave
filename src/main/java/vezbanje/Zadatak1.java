642b2e027a1441babaea33298dc88190bb33a7b6
Jovana Cvetanovic <jovana.cvetanovic0302@gmail.com>
2022-12-14 16:12:20 +0100

    Zadatak sa Slack-a

---
+++ src/main/java/vezbanje/Zadatak1.java
@@ -0,0 +1,52 @@
+package vezbanje;
+
+public class Zadatak1 {
+    public static void main(String[] args) {
+
+        boolean isFilled;
+        isFilled = false;
+
+        System.out.println("                    @@@@@");
+        System.out.println("                   @@   @@" + " " + isFilled);
+        System.out.println("                  @@@    @@" + " " + isFilled);
+        System.out.println("                 @@     @@" + " " + isFilled);
+        System.out.println("                @@@     @@" + " " + isFilled);
+        System.out.println("               @@@      @@" + " " + isFilled);
+        System.out.println("              @@        @@@@@@@@@@@@@ " + " " + isFilled);
+        System.out.println("  @@@@@@@@@ @@@                    @@@" + " " + isFilled);
+        System.out.println("@@         @@                       @@" + " " + isFilled);
+        System.out.println("@@         @@                       @@" + " " + isFilled);
+        System.out.println("@@         @@                       @@" + " " + isFilled);
+        System.out.println("@@         @@                       @@" + " " + isFilled);
+        System.out.println("@@         @@                      @@" + " " + isFilled);
+        System.out.println("@@         @@                     @@" + " " + isFilled);
+        System.out.println("@@         @@                    @@" + " " + isFilled);
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+
+        System.out.println("Picture is filled: " + isFilled);
+
+        isFilled = true;
+
+        System.out.println("                    @@@@@");
+        System.out.println("                   @@***@@" + " " + isFilled);
+        System.out.println("                  @@@****@@" + " " + isFilled);
+        System.out.println("                 @@*****@@" + " " + isFilled);
+        System.out.println("                @@@*****@@" + " " + isFilled);
+        System.out.println("               @@@******@@" + " " + isFilled);
+        System.out.println("              @@********@@@@@@@@@@@@@ " + " " + isFilled);
+        System.out.println("  @@@@@@@@@ @@@********************@@@" + " " + isFilled);
+        System.out.println("@@*********@@***********************@@" + " " + isFilled);
+        System.out.println("@@*********@@***********************@@" + " " + isFilled);
+        System.out.println("@@*********@@***********************@@" + " " + isFilled);
+        System.out.println("@@*********@@***********************@@" + " " + isFilled);
+        System.out.println("@@*********@@**********************@@" + " " + isFilled);
+        System.out.println("@@*********@@*********************@@" + " " + isFilled);
+        System.out.println("@@*********@@********************@@" + " " + isFilled);
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+
+        System.out.println("Picture is filled: " + isFilled);
+
+
+
+    }
+}
